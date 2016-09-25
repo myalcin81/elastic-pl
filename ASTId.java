aad37a3eb7430c8d54e7574d9e7bd0a0c8528496
Peck <peck@localhost>
2016-09-25 23:33:06 +0200

    fix

--- ASTId.java
+++ ASTId.java
@@ -41,9 +41,12 @@ public class ASTId extends SimpleNode {
   public void interpret()
   {
      jjtGetChild(0).interpret();
-     Integer topint = (Integer)stack[top];
-     if(topint<0 || topint>64000)
-      topint = 0;
+     Integer topint = 0;
+     if(stack[top] instanceof Integer)
+       topint = (Integer)stack[top];
+     else if(stack[top] instanceof Boolean)
+       topint = ((Boolean)stack[top] == true)?1:0;
+
      String name = "m[" + topint + "]";
      stack[top] = symtab.get(name);
   }
